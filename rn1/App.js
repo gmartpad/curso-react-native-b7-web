47e4b67cd7ce9f0c2bf115b2a20985fc84e4c958
Gabriel Martins Padoin <gabrielmpwork@gmail.com>
2020-10-15 00:09:07 -0300

    Estilização com StyledComponents

--- rn1/App.js
+++ rn1/App.js
@@ -1,26 +1,40 @@
 import React from 'react'
 import { SafeAreaView, View, Text, StyleSheet } from 'react-native'
+import styled from 'styled-components/native'
+
+const Page = styled.SafeAreaView`
+
+  background-color: #00f;
+  flex: 1;
+
+`
+const Texto = styled.Text`
+
+  color: ${props => props.cor};
+  font-size: 20px;
+
+`
 
 export default () => {
   return (
-    <SafeAreaView style={styles.page}>
-      <Text style={styles.texto}>Olá, Mundo!</Text>
-      <Text style={[styles.texto, {fontSize: 16}]}>Olá, Galáxia!</Text>
-    </SafeAreaView>
+    <Page>
+      <Texto cor="#0f0" >Olá, Mundo!</Texto>
+      <Texto cor="#f00" >Olá, Galáxia!</Texto>
+    </Page>
   )
 }
 
-const styles = StyleSheet.create({
- page: {
-   width: 200,
-   height: 200,
-   backgroundColor: '#00ffff'
- },
- texto: {
-   fontSize: 20,
-   fontStyle: 'italic'
- } 
-})
+// const styles = StyleSheet.create({
+//  page: {
+//    width: 200,
+//    height: 200,
+//    backgroundColor: '#00ffff'
+//  },
+//  texto: {
+//    fontSize: 20,
+//    fontStyle: 'italic'
+//  } 
+// })
 
 //-----------------------------------------------------
 

eef7f8d4932cf5f46bfd49ac998c72f2bafaa160
Gabriel Martins Padoin <gabrielmpwork@gmail.com>
2020-10-18 20:28:06 -0300

    Projeto: Calculadora de Gorjeta (1/3)

--- rn1/App.js
+++ rn1/App.js
@@ -2,69 +2,47 @@ import React, { useState } from 'react'
 import { SafeAreaView, View, Text, StyleSheet, Button } from 'react-native'
 import styled from 'styled-components/native'
 
-const Input = styled.TextInput`
-  width: 200px;
-  height: 40px;
-  border: 1px solid #000;
-`
-
-const Quadrado = styled.View`
-  width: 200px;
-  height: 200px;
-  justify-content: center;
-  align-items: center;
-  border: 3px dashed #000 !important;
-  margin-top: 30px;
-`
-
-const Hello = ({ frase }) => {
-
-  const [name, setName] = useState('Gabriel Martins Padoin')
-  const [mostrar, setMostrar] = useState(false)
-
-  const mudarTexto = (texto) => {
-    setName(texto)
-  }
-
-  const handleClick = () => {
-    setMostrar(!mostrar);
-  }
-
-  return(
-    <View>
-
-      <Input 
-        value={name}
-        onChangeText={mudarTexto} 
-        placeholder="passa o radinho"
-      />
-
-      <Button onPress={handleClick} title={mostrar ? "Ocultar Nome" : "Mostrar Nome"} />
-
-      {mostrar && 
-      
-        <Quadrado>
-          <Text>{name}</Text>
-        </Quadrado>
-      
-      }
-
-    </View>
-  )
-}
-
 const Page = styled.SafeAreaView`
 
   flex: 1;
-  justify-content: center;
+  /* justify-content: center; */
   align-items: center;
 
 `
+
+const HeaderText = styled.Text`
+
+  font-size: 25px;
+  /* flex: 1; */
+  margin-top: 20px;
+
+`
+
+const Input = styled.TextInput`
+
+  width: 90%;
+  height: 50px;
+  font-size: 18px;
+  background: #EEE;
+  margin-top: 20px;
+  padding: 0 10px;
+  border-radius: 10px;
+
+`
+
 export default () => {
 
+  const [bill, setBill] = useState('0')
+  
   return (
     <Page>
-      <Hello frase="dai mermão passa o radinho"/>  
+      <HeaderText>Calculadora de Gorjeta</HeaderText>
+      <Input 
+        placeholder='Quanto deu a conta?'
+        keyboardType='numeric'
+        value={bill}
+        onChangeText={n=>setBill(n)}
+      />
     </Page>
   )
 }
